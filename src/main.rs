3843f3c0a6010197a7a62fff50b248fb0c07ad93
Mohamed Amjar <amjar.mohamed.rh@gmail.com>
2024-10-21 03:44:27 +0100

    day 2-1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // use rand::Rng;
 // use std::io;
+use druid::{AppLauncher, Widget, WindowDesc, widget::Label};
 
+
+fn build_ui() -> impl Widget<()> {
+    Label::new("Hello, World!")
+}
 fn main() {
-    let k = 55;
-    //FIXME: add random number
- let s= 555;
-    println!("Guess the number!{} - {}", k, s);
-
+   let main_window= WindowDesc::new(build_ui()) ;
+   AppLauncher::with_window(main_window).launch(()).expect("launch failed");
 }
 
